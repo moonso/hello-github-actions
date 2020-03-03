aeecc9c868a23c7c1105922d1aad777a91f7c640
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-03-03 15:08:50 +0100

    Adds mongodb test

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,3 +1,6 @@
+import pymongo
+
+
 def test_assertion():
     # GIVEN a variable
     a = 1
@@ -5,3 +8,15 @@ def test_assertion():
 
     # THEN assert it is a integer
     assert isinstance(a, int)
+
+
+def test_mongodb():
+    # GIVEN a client
+    client = pymongo.MongoClient()
+    # WHEN creating a database and inserting into collection
+    db = client.test
+    post = {"name": "Me"}
+    db.person.insert_one(post)
+    # THEN assert the doc is there
+    res = db.person.find_one()
+    assert res["name"] == post["name"]
